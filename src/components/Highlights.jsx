070a778fd5ced0609d1030cb728962b2d2415a8f
Chris <moltencore95@hotmail.de>
2022-11-16 15:56:36 +0100

    building page with reusable components

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import Highlight from './ui/highlight'
 
-export default function Highlights() {
+ function Highlights() {
   return (
-    <div>Highlights</div>
-  )
+    <section id="highlights">
+        <div className="container">
+            <h2 className="section__title">
+                Why choose <span className="purple">Library</span>
+            </h2>
+            <div className="highlight__wrapper">
+                <Highlight icon ={<FontAwesomeIcon icon="bolt" />} title="Easy and Quick" para="Get access to the book you purchased only instantly." />
+                <Highlight icon ={<FontAwesomeIcon icon="book-open" />} title="10.000+ Books" para="Library has books in all your favourite categories." />
+                <Highlight icon ={<FontAwesomeIcon icon="tags" />} title="Affordable" para="Get your hands on popular books for as little as $10." />
+            </div>
+        </div>
+    </section>
+  );
 }
+
+export default Highlights;
