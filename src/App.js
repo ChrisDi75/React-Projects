c88654eedac79f473ebb4574d0bb501f6c6c59cc
Chris <moltencore95@hotmail.de>
2022-11-18 20:24:57 +0100

    sorting added through react

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from "./components/ui/Footer";
 import {  BrowserRouter as Router, Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
 
 function App() {
   return (
@@ -12,7 +14,8 @@ function App() {
       <Route />
         <Nav />
         <Route path="/" exact component={Home} />
-        <Route Path="/books" component={Books} />
+        <Route Path="/books" render={() => <Books books={books} />} />
+        <Route path="/books/1" render={() => <BookInfo books={books} />} />
         <Footer />
     </div>
     </Router>
---
+++ src/pages/BookInfo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function BookInfo( { books} ) {
+  return (
+    <div className="books__body">
+        <div className="books__main">
+            <div className="books__container">
+                <div className="row">
+                    <div className="book__selected--top"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  )
+}
